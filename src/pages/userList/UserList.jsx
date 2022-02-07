77484bfae07b10285a65d58d3abab0c3fb648a62
arch-design <arch.architectural@gmail.com>
2022-02-07 18:57:41 +0530

    userlist halfway

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,46 +1,68 @@
-import * as React from 'react';
+import * as React from "react";
 import "./userList.css";
-import { DataGrid } from '@mui/x-data-grid';
-import {userRows} from "../../dummyData.js";
-import {useState} from 'react'; 
+import { DataGrid } from "@mui/x-data-grid";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import { userRows } from "../../dummyData.js";
+import { useState } from "react";
 
 function UserList() {
-
   const [rows, setRows] = useState(userRows);
 
-
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    { field: 'username', headerName: 'Username', width: 200 },
-    { field: 'email', headerName: 'Email', width: 200 },
+    { field: "id", headerName: "ID", width: 90 },
     {
-      field: 'status',
-      headerName: 'Status',
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className='userListUser'>
+            <img className='userListAvatar' src={params.row.avatar} alt='' />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
       width: 120,
     },
     {
-      field: 'transaction',
-      headerName: 'Transaction volume',
+      field: "transaction",
+      headerName: "Transaction volume",
       width: 160,
     },
-
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <button className='userListEdit'>Edit</button>
+            <DeleteOutlineIcon />
+          </>
+        );
+      },
+    },
   ];
 
-
-
-
-  return <div className='userList'>User List
-      <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
+  return (
+    <div className='userList'>
+      User List
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
     </div>
-  
-  </div>;
+  );
 }
 
 export default UserList;
