75c77722dd2a0d2f54e13ad5a16fb0a258cc9f71
keeth-office <keerthi.mac@gmail.com>
2022-02-08 12:26:56 +0530

    user page halfway

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,16 +1,63 @@
 import React from "react";
 import "./user.css";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import LocationSearchingIcon from "@material-ui/icons/LocationSearching";
 
 function User() {
   return (
-    <div className='user'>
-      <div className='userTitleContainer'>
-        <h1 className='userTitle'>Edit User</h1>
-        <button className='userAddButton'>Create</button>
+    <div className="user">
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <button className="userAddButton">Create</button>
       </div>
-      <div className='userContainer'>
-        <div className='userShow'>User Show</div>
-        <div className='userUpdate'>User Update</div>
+      <div className="userContainer">
+        <div className="userShow">
+          <div className="userShowTop">
+            <div className="userShowImage">
+              <img
+                src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt="Avatar"
+                className="userShowImage"
+              />
+            </div>
+            <div className="userShowTopTitle">
+              <span className="UserShowUserName">Hasitha Rathnayake</span>
+              <span className="UserShowUserTitle">Quantity Surveyor</span>
+            </div>
+          </div>
+          <div className="userShowBottom">
+            <span className="userShowBottomTitle">Account Details</span>
+            <div className="userShowInfo">
+              <PermIdentityIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">hasi99</span>
+            </div>
+            <div className="userShowInfo">
+              <CalendarTodayIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">1989.12.03</span>
+            </div>
+
+            <span className="userShowBottomTitle">Contact Details</span>
+            <div className="userShowInfo">
+              <PhoneAndroidIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">+94 71 6521 436</span>
+            </div>
+            <div className="userShowInfo">
+              <MailOutlineIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">Keerthi.mac@gmail.com</span>
+            </div>
+            <div className="userShowInfo">
+              <LocationSearchingIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">Sri Lanka | Kandy</span>
+            </div>
+          </div>
+        </div>
+        <div className="userUpdate">
+          <div className="userUpdateTop"></div>
+          <div className="userUpdateBottom"></div>
+        </div>
       </div>
     </div>
   );
